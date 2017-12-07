35a2d020838551ac90eb89299e6006c92048e7a3
Icey Lee <iceyleemj@gmail.com>
2017-12-07 09:19:15 +0800

    数据和作图分开

--- 6Orgdb-GO.R
+++ 6Orgdb-GO.R
@@ -4,6 +4,8 @@
 # biocLite()
 
 
+rm=list(ls())
+
 library(clusterProfiler)
 library(DOSE)
 
@@ -49,56 +51,3 @@ info_go_MF <- enrichGO(gene = gene_id,
 write.table(as.data.frame(info_go_BP@result), file="GO_BP_out.txt",quote=F,row.names = F,sep = "\t")
 write.table(as.data.frame(info_go_CC@result), file="GO_CC_out.txt",quote=F,row.names = F,sep = "\t")
 write.table(as.data.frame(info_go_MF@result), file="GO_MF_out.txt",quote=F,row.names = F,sep = "\t")
-
-ego_CC <- as.data.frame(info_go_CC)
-ego_BP <- as.data.frame(info_go_BP)
-ego_MF <- as.data.frame(info_go_MF)
-
-#get gene sum number
-tep_num1 = ego_CC[1,3]
-tep_num2 = strsplit(tep_num1 , "/")
-gene_num_CC = as.numeric(tep_num2[[1]][2])
-
-tep_num1 = ego_MF[1,3]
-tep_num2 = strsplit(tep_num1 , "/")
-gene_num_MF = as.numeric(tep_num2[[1]][2])
-
-tep_num1 = ego_BP[1,3]
-tep_num2 = strsplit(tep_num1 , "/")
-gene_num_BP = as.numeric(tep_num2[[1]][2])
-
-
-
-#p value top 15 select
-ego_CC_df <- as.data.frame(info_go_CC)[1:3, c(2:3, 9)] %>%
-  transform(onco = "CC", GeneRatio = Count / gene_num_CC * 100)
-
-ego_BP_df <- as.data.frame(info_go_BP)[1:10, c(2:3, 9)] %>%
-  transform(onco = "BP", GeneRatio = Count / gene_num_BP * 100)
-
-ego_MF_df <- as.data.frame(info_go_MF)[1:10, c(2:3, 9)] %>%
-  transform(onco = "MF", GeneRatio = Count / gene_num_MF * 100)
-
-
-##bind three onco
-ego_three <- rbind(ego_CC_df, ego_BP_df, ego_MF_df)
-View(ego_three)
-
-##plot bar
-#########NEED CHANGE limits
-library(ggplot2)
-p <- ggplot(ego_three, aes(y = GeneRatio, x = Description)) +
-  geom_bar(stat = "identity", aes(fill = onco), alpha = 0.7) +
-  facet_grid(onco ~ ., scales = "free", space = "free") +
-  coord_flip()  +
-  #scale_y_continuous(limits = c(0, 70))+
-  scale_fill_discrete(name = "Ontology", labels = c("Cellular component","Biological process", "Molecular function")) +
-  theme_light() +
-  theme(axis.text = element_text(size = 6, face = "bold"), legend.text = element_text(size = 6, face = "bold")) +
-  labs(y = "Percentage of Genes", x = "Term")
-
-pdf(file="GO_barplot.pdf")
-p
-dev.off()
-
-#ggsave("COG.pdf",p, width=7, height=7, units="in")
