f3f81e152e9f0dcde90c12556fcd2facdfa3d9bd
Icey Lee <iceyleemj@gmail.com>
2017-12-07 09:19:20 +0800

    update

--- learning.R
+++ learning.R
@@ -23,3 +23,20 @@ source("http://bioconductor.org/biocLite.R")
 biocLite("DESeq")
 biocLite("DESeq2")
 biocLite("edgeR")
+
+#修改levels顺序，按照表格的顺序（逆序），而不是字母表。作图的轴线出现的顺序会受影响	
+ego_three$Description<- factor(ego_three$Description, order=TRUE, levels=ego_three$Description)
+
+p <- p + theme(panel.spacing = unit(0, "lines"))
+
+ggplot
+p+theme(axis.text = element_text(size = 8，face="bold"), legend.text = element_text(size = 8)) 
+
+
+
+
+
+###可以按照表格显示的term顺序作图 ---序列是从小到大的
+##如何让表格显示的顺序是BP CC MF呢（level需要修改）
+ego_three$term<- factor(ego_three$term, order=TRUE, levels=rev(ego_three$term))
+# ego_three$class<- factor(ego_three$class, order=TRUE, levels=c("MF","CC","BP"))
