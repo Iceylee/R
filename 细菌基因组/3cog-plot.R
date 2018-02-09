fb4fac73f2706d0dbb99bdef94ed856417318113
Icey Lee <iceyleemj@gmail.com>
2018-02-09 10:25:25 +0800

    调整legend symbol 大小

--- 细菌基因组/3cog-plot.R
+++ 细菌基因组/3cog-plot.R
@@ -1,5 +1,3 @@
-
-
 library(dplyr)
 cog.count = read.table(file = "cog_count_fun.id", sep = "\t", header = F, stringsAsFactors = F)
 
@@ -21,25 +19,25 @@ cog.count2$term <- as.factor(cog.count2$term)
 
 library(ggplot2)
 p <- ggplot(cog.count2,aes(x = class, y = count)) +
-#fill用term列，这样legend就直接打出term了
+  #fill用term列，这样legend就直接打出term了
   geom_bar(aes(fill = term),stat= "identity")+
   #去掉网格和背景颜色。调整lengend标签文件大小
-  theme_light() +theme(panel.border = element_blank(),panel.grid.major = element_blank(),panel.grid.minor = element_blank(),axis.line = element_line(colour = "black"),legend.text=element_text(size=4)) +
+  theme_light() +theme(panel.border = element_blank(),panel.grid.major = element_blank(),panel.grid.minor = element_blank(),axis.line = element_line(colour = "black"),legend.text=element_text(size=8)) +
   #三个标签 x轴 y轴 legend（fill）
   labs(y = "Number of matched genes", x = "Function class",fill = "Function Classification") +
   #颜色
-  scale_color_manual(values=rainbow(23))+
-  #legend从默认的两列调整为一列。
-  guides(fill=guide_legend(ncol=1))
-  
+  scale_color_manual(values=rainbow(23)) +
+  #legend从默认的两列调整为一列。 symbol size small
+  guides(fill=guide_legend(ncol=1,override.aes = list(size = 0.2)))
+
 #调整legend symbol的大小。 
 #   guides(fill=guide_legend(override.aes = list(width = 0.5))) +
 #   guides(color=guide_legend(override.aes=list(fill=NA))) +
 #   theme(legend.key=element_blank()) 
 
-p
+p 
+ 
+  
 
 #存储pdf的大小
-ggsave("COG_barplot.pdf",p, width=7, height=7, units="in")
-
-
+ggsave("COG_barplot.pdf",p, width=10, height=7, units="in")
